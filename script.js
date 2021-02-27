4f97ae158abff69b76143d3fc81443a56ec8f6ce
R N <cekyndant2@mail.ru>
2021-02-27 19:52:42 +0300

    0.5

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
-let showPopupButton  = document.querySelector('.profile-char__edit')
-let popup            = document.querySelector('.popup')
-let closePopupButton = document.querySelector('.popup__close')
-let textup   = document.querySelector('.profile-char__title').innerText
-let textdo = document.querySelector('.profile-char__subtitle').innerText
-let inputUp  = document.querySelector('.popup__name')
-let inputDo  = document.querySelector('.popup__work')
-let butnSave = document.querySelector('.popup__save-title')
+let showBt  = document.querySelector('.profile-char__edit')
+let popup   = document.querySelector('.popup')
+let closeBt = document.querySelector('.popup__close')
+let textup  = document.querySelector('.profile-char__title')
+let textdo  = document.querySelector('.profile-char__subtitle')
+let saveBt  = document.querySelector('.popup__save')
+let inputUp = document.getElementById('popup-up')
+let inputDo = document.getElementById('popup-do')
 
-inputUp.value = textup;
-inputDo.value = textdo;
+inputUp.value = textup.textContent
+inputDo.value = textdo.textContent
 
-showPopupButton.addEventListener('click' , togglePopup);
-closePopupButton.addEventListener('click' , togglePopup);
-butnSave.addEventListener('click' , showL);
+showBt.addEventListener('click' , togglePopup);
+closeBt.addEventListener('click' , togglePopup);
+saveBt.addEventListener('click' , showPopup);
 
-function togglePopup() {
-  popup.classList.toggle('popup__open')
+function togglePopup(event) {
+  popup.classList.toggle('popup_open')
 }
-
-function showL() {
-  document.getElementById("textUp").innerHTML = inputUp.value
-  document.getElementById("textDo").innerHTML = inputDo.value
+function showPopup(event) {
+  event.preventDefault()
+  textup.textContent = inputUp.value
+  textdo.textContent = inputDo.value
   togglePopup()
 }
-
-//console.log(inputUp);
-//console.log(document.getElementById("textUp").innerHTML);
