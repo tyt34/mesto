aa65737ed8e4a40496a1bb9e037600299a00559d
tyt34 <30603694+tyt34@users.noreply.github.com>
2021-03-12 18:48:08 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -34,8 +34,8 @@ function togglePopup(popup) {
 
 function deleteCard(event) {
   const target = event.target;
-	const currentCard = target.closest('.place');
-	currentCard.remove();
+  const currentCard = target.closest('.place');
+  currentCard.remove();
 }
 
 function createLike(event) {
@@ -59,7 +59,7 @@ function openPopupEdit(event) {
 }
 
 function createCard(item){
-	const newItem = templateElement.content.cloneNode(true);
+  const newItem = templateElement.content.cloneNode(true);
   const picTemplate = newItem.querySelector('.place__img');
   const textTemplate = newItem.querySelector('.place__title');
   picTemplate.src = item.link
@@ -71,15 +71,15 @@ function createCard(item){
   buttonLike.addEventListener('click', createLike)
   buttonDel.addEventListener('click', deleteCard)
   img.addEventListener('click', openImg)
-	return newItem;
+  return newItem;
 }
 
 function renderList() {
-	const result = initialCards.map(function(item) {
-		const newCard = createCard(item);
-		return newCard;
-	});
-	container.append(...result)
+  const result = initialCards.map(function(item) {
+    const newCard = createCard(item);
+    return newCard;
+  });
+  container.append(...result)
 }
 
 function editProfile(event) {
